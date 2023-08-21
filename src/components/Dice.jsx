98aaa641fd7250f6dff3b8ebfec1c0435a7c582a
kamal-y <kamal9967222@gmail.com>
2023-08-21 22:26:09 +0530

    button css

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiceOne,faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons';
+import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons';
 
 function Dice() {
-	const [numberOnDices, setNumberOnDices] = useState([4,3]);
+	const [numberOnDices, setNumberOnDices] = useState([4, 3]);
 	const [beat, setBeat] = useState(true);
 
 	const diceIcons = [
@@ -14,23 +14,27 @@ function Dice() {
 		faDiceFour,
 		faDiceFive,
 		faDiceSix
-];
-	const getRandomDiceNumber = ()=> Math.floor(Math.random()*5)+1;
-	const handleClick = ()=> {
+	];
+	const getRandomDiceNumber = () => Math.floor(Math.random() * 5) + 1;
+	
+	const handleClick = () => {
+
 		setBeat(prev => !prev);
-    const interval = setInterval(() => {
-        setNumberOnDices([getRandomDiceNumber(), getRandomDiceNumber()]);
-    }, 50);
-    setTimeout(() => {
-        clearInterval(interval);
-        setTimeout(()=> setBeat(prev => !prev),2000);
-    }, 500);
+		const interval = setInterval(() => {
+			setNumberOnDices([getRandomDiceNumber(), getRandomDiceNumber()]);
+		}, 50);
+
+		setTimeout(() => {
+			clearInterval(interval);
+			setTimeout(() => setBeat(prev => !prev), 2000);
+		}, 500);
 	}
+
 	return (
 		<div className="flex justify-center my-2">
 			<div className='flex' onClick={handleClick}>
-				<FontAwesomeIcon key={0} icon={diceIcons[numberOnDices[0]]} beat={beat} className='text-[3rem] m-2'/>
-				<FontAwesomeIcon key={1} icon={diceIcons[numberOnDices[1]]} beat={beat} className='text-[3rem] m-2'/>
+				<FontAwesomeIcon key={0} icon={diceIcons[numberOnDices[0]]} beat={beat} className='text-[3rem] m-2' />
+				<FontAwesomeIcon key={1} icon={diceIcons[numberOnDices[1]]} beat={beat} className='text-[3rem] m-2' />
 			</div>
 		</div>
 	)
--- src/components/PlayerMenu.jsx
+++ src/components/PlayerMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { MyContext } from "../context/MyContext";
 import { useState } from "react";
 
@@ -7,9 +7,19 @@ const PlayerMenu = (props) => {
   let setPlayerCount = props.setPlayerCount;
   let setPlayerSelected = props.setPlayerSelected;
   let playerSelected = props.playerSelected;
+
+  const [buttonClicked, setButtonClicked] = useState(2);
+
+  useEffect(() => {
+    // This effect runs whenever buttonClicked state changes
+    console.log("button clicked ->", buttonClicked);
+  }, [buttonClicked]);
+
   function changeHandler(event) {
-    setPlayerCount(event.target.value);
-    console.log("plaer count value " + event.target.value)
+    const totalPlayerSelected = event.target.value;
+    setPlayerCount(totalPlayerSelected);
+    setButtonClicked(totalPlayerSelected);
+    console.log("player count value " + event.target.value);
   }
 
   // const [LabelSelected, setLabelSelected] = useState(2);
@@ -29,7 +39,7 @@ const PlayerMenu = (props) => {
       <div className="mt-2 text-white    text-2xl p-2 font-medium  bg-[#22223b]">Select Number of Players</div>
       <div className="flex flex-row justify-evenly  -translate-x-5 text-white font-semibold">
         <input
-          onChange={changeHandler}
+          onClick={changeHandler}
           className="hidden"
           type="radio"
           id="2"
@@ -37,25 +47,37 @@ const PlayerMenu = (props) => {
           name="playerCount" />
 
         <label htmlFor={2}
-          className={`cursor-pointer    mt-6 w-1/5 aspect-square p-4 text-4xl  bg-opacity-5  bg-white`}>2</label>
+          className={`cursor-pointer  rounded-md  mt-6 w-1/5 aspect-square p-4 text-4xl  ${
+            buttonClicked == '2'
+              ? "bg-green-500 text-white border-b-4 border-green-700"
+              : "bg-white bg-opacity-5"
+          }`}>2</label>
         <input
-          onChange={changeHandler}
+          onClick={changeHandler}
           className="hidden"
           type="radio"
           id="3"
           value={3}
           name="playerCount" />
         <label htmlFor={3}
-          className={`cursor-pointer    mt-6 w-1/5 aspect-square p-4 text-4xl  bg-opacity-5  bg-white`}>3</label>
+          className={`cursor-pointer  rounded-md  mt-6 w-1/5 aspect-square p-4 text-4xl  ${
+            buttonClicked == '3'
+              ? "bg-green-500 text-white border-b-4 border-green-700"
+              : "bg-white bg-opacity-5"
+          }`}>3</label>
         <input
-          onChange={changeHandler}
+          onClick={changeHandler}
           className="hidden"
           type="radio"
           id="4"
           value={4}
           name="playerCount" />
         <label htmlFor="4"
-          className={`cursor-pointer    mt-6 w-1/5 aspect-square p-4 text-4xl  bg-opacity-5  bg-white`}>4</label>
+          className={`cursor-pointer  rounded-md  mt-6 w-1/5 aspect-square p-4 text-4xl  ${
+            buttonClicked == '4'
+              ? "bg-green-500 text-white border-b-4 border-green-700"
+              : "bg-white bg-opacity-5"
+          }`}>4</label>
       </div>
       <div className="flex justify-center items-center border-white mt-4">
         <button className="m-4 px-5 text-3xl font-medium  active:border-2 active:border-white   text-black bg-green-300 p-2 rounded-xl " onClick={clickHandler}>
