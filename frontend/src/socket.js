f89b48401c31933627337ff88b7d5587f7e1fe2d
skj_og <95894160+Jha-Shubham19@users.noreply.github.com>
2024-05-24 12:59:26 +0530

    transport websocket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,4 +3,6 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? 'https://vyapaar-server.vercel.app' : 'http://localhost:5000';
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ['websocket']
+  });
